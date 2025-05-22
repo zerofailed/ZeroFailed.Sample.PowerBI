8fc22a00d045ec816814d52a110913aa2364e57b
Jessica Hill <Jessica.Hill@endjin.com>
2025-05-22 16:30:57 +0100

    Set up repo

---
+++ .zf/config.ps1
@@ -0,0 +1,80 @@
+<#
+This example demonstrates a software build process using the 'ZeroFailed.Build.DotNet' extension
+to provide the features needed when building a .NET solutions.
+#>
+
+$zerofailedExtensions = @(
+    @{
+        # References the extension from its GitHub repository. If not already installed, use latest version from 'main' will be downloaded.
+        Name = "ZeroFailed.Build.DotNet"
+        GitRepository = "https://github.com/zerofailed/ZeroFailed.Build.DotNet"
+        GitRef = "main"
+    }
+)
+
+# Load the tasks and process
+. ZeroFailed.tasks -ZfPath $here/.zf
+
+#
+# Build process configuration
+#
+#
+# Build process control options
+#
+$SkipInit = $false
+$SkipVersion = $false
+$SkipBuild = $false
+$CleanBuild = $Clean
+$SkipTest = $false
+$SkipTestReport = $false
+$SkipAnalysis = $false
+$SkipPackage = $false
+
+$SolutionToBuild = (Resolve-Path (Join-Path $here "./src/SampleDotNet.sln")).Path
+$IncludeAssembliesInCodeCoverage = "SampleDotNet*"
+$TargetFrameworkMoniker = "net8.0" # prevent GHA trying to run tests under .NET Framework 4.5 - Linux agents now lack mono
+$ProjectsToPublish = @(
+    # simple syntax for basic publishing features
+    "$here/src/SampleDotNet.Web/SampleDotNet.Web.csproj"
+    "$here/src/SamplePlugin/SamplePlugin.csproj"
+
+    # richer syntax to support multi-target publishing and AOT features
+    @{
+        Project = "$here/src/SampleDotNet.Cli/SampleDotNet.Cli.csproj"
+        RuntimeIdentifiers = @("win-x64", "linux-x64")
+        SelfContained = $false
+        SingleFile = $false
+        Trimmed = $false
+        ReadyToRun = $false
+    }
+)
+$NuSpecFilesToPackage = @(
+    "src/SamplePlugin/SamplePlugin.nuspec"
+)
+
+
+# Customise the build process
+task . FullBuild
+
+#
+# Build Process Extensibility Points - uncomment and implement as required
+#
+
+# task RunFirst {}
+# task PreInit {}
+# task PostInit {}
+# task PreVersion {}
+# task PostVersion {}
+# task PreBuild {}
+# task PostBuild {}
+# task PreTest {}
+# task PostTest {}
+# task PreTestReport {}
+# task PostTestReport {}
+# task PreAnalysis {}
+# task PostAnalysis {}
+# task PrePackage {}
+# task PostPackage {}
+# task PrePublish {}
+# task PostPublish {}
+# task RunLast {}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# ZeroFailed.Sample.PowerBI
+# ZeroFailed.Sample.DotNet
+
+A sample repository demonstrating how to use the [ZeroFailed.Build.DotNet extension](https://github.com/zerofailed/ZeroFailed.Build.DotNet) to easily setup a fully-featured build process for .NET solutions.
+
+The current features of this extension can be found [here]().
+
+## Example Projects
+
+* `SampleDotNet.Cli` - a console app that gets packaged via `dotnet publish` and leverages [AOT](https://learn.microsoft.com/en-us/dotnet/core/deploying/native-aot) features
+* `SampleDotNet.Lib` - a class library that is packaged for NuGet via `dotnet pack`
+* `SampleDotNet.Tests` - an NUnit test project demonstrating the testing-related features (e.g. execution, code coverage, reports)
+* `SampleDotNet.Web` - a web app that get packaged for deployment via `dotnet publish`
+* `SamplePlugin` - demonstrates building NuGet packages via a `.nuspec` file that references the output from `dotnet publish`
+
