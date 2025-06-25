06f416f72959e2c8029cf9fc60b76fd21248b5e8
Jessica Hill <Jessica.Hill@endjin.com>
2025-06-25 12:13:10 +0100

    WIP working on able to consume to deploy power bi tasks

--- .zf/config.ps1
+++ .zf/config.ps1
@@ -7,8 +7,9 @@ $zerofailedExtensions = @(
     @{
         # References the extension from its GitHub repository. If not already installed, use latest version from 'main' will be downloaded.
         Name = "ZeroFailed.Deploy.PowerBI"
-        GitRepository = "https://github.com/zerofailed/ZeroFailed.Deploy.PowerBI"
-        GitRef = "main"
+        # GitRepository = "https://github.com/zerofailed/ZeroFailed.Deploy.PowerBI"
+        # GitRef = "main"
+        Path = "C:\Users\JessicaHill\Repos\ZeroFailed.Deploy.PowerBI\module"
     }
 )
 
@@ -19,7 +20,7 @@ $cloudConnection = @(
     @{
         displayName = "My Shared Cloud Connection"
         connectionType = "AzureBlobs"
-        parameters = @(
+        parameters = {@(
             @{
                 dataType = "Text"
                 name = "domain"
@@ -30,9 +31,9 @@ $cloudConnection = @(
                 name = "account"
                 value = $DeploymentConfig.storageAccountName
             }
-        )
+        )}
         # servicePrincipalClientId = $app.AppId
-        servicePrincipalSecret = $DeploymentConfig.storageConnectionCloudSecret | ConvertTo-SecureString -AsPlainText
+        servicePrincipalSecret = {$DeploymentConfig.storageConnectionCloudSecret | ConvertTo-SecureString -AsPlainText}
         tenantId = "0f621c67-98a0-4ed5-b5bd-31a35be41e29"
     }
 )
---
+++ deploy.ps1
@@ -0,0 +1,89 @@
+#Requires -Version 7
+<#
+.SYNOPSIS
+    Runs a .NET flavoured build process.
+.DESCRIPTION
+    This script was scaffolded using a template from the ZeroFailed project.
+    It uses the InvokeBuild module to orchestrate an opinionated software build process for .NET solutions.
+.EXAMPLE
+    PS C:\> ./build.ps1
+    Downloads any missing module dependencies (ZeroFailed & InvokeBuild) and executes
+    the build process.
+.PARAMETER Tasks
+    Optionally override the default task executed as the entry-point of the build.
+.PARAMETER ZfModulePath
+    The path to import the ZeroFailed module from. This is useful when testing pre-release
+    versions of ZeroFailed that are not yet available in the PowerShell Gallery.
+.PARAMETER ZfModuleVersion
+    The version of the ZeroFailed module to import. This is useful when testing pre-release
+    versions of ZeroFailed that are not yet available in the PowerShell Gallery.
+.PARAMETER InvokeBuildModuleVersion
+    The version of the InvokeBuild module to be used.
+#>
+[CmdletBinding()]
+param (
+    [Parameter(Position=0)]
+    [string[]] $Tasks = @("."),
+
+    [Parameter(Mandatory = $true)]
+    [string] $Environment,
+
+    [Parameter()]
+    [string] $EnvironmentConfigName = $Environment,
+
+    [Parameter()]
+    [string] $ConfigPath,
+
+    [Parameter()]
+    [string] $ZfModulePath,
+
+    [Parameter()]
+    [string] $ZfModuleVersion = "1.0.5",
+
+    [Parameter()]
+    [version] $InvokeBuildModuleVersion = "5.12.1"
+)
+$ErrorActionPreference = 'Stop'
+$here = Split-Path -Parent $PSCommandPath
+
+#region InvokeBuild setup
+# This handles calling the build engine when this file is run like a normal PowerShell script
+# (i.e. avoids the need to have another script to setup the InvokeBuild environment and issue the 'Invoke-Build' command )
+if ($MyInvocation.ScriptName -notlike '*Invoke-Build.ps1') {
+    Install-PSResource InvokeBuild -Version $InvokeBuildModuleVersion -Scope CurrentUser -TrustRepository -Verbose:$false | Out-Null
+    try {
+        Invoke-Build $Tasks $MyInvocation.MyCommand.Path @PSBoundParameters
+    }
+    catch {
+        Write-Host -f Yellow "`n`n***`n*** Build Failure Summary - check previous logs for more details`n***"
+        Write-Host -f Yellow $_.Exception.Message
+        Write-Host -f Yellow $_.ScriptStackTrace
+        exit 1
+    }
+    return
+}
+#endregion
+
+#region Initialise build framework
+$splat = @{ Force = $true; Verbose = $false}
+Import-Module Microsoft.PowerShell.PSResourceGet
+if (!($ZfModulePath)) {
+    Install-PSResource ZeroFailed -Version $ZfModuleVersion -Scope CurrentUser -TrustRepository | Out-Null
+    $ZfModulePath = "ZeroFailed"
+    $splat.Add("RequiredVersion", ($ZfModuleVersion -split '-')[0])
+}
+else {
+    Write-Host "ZfModulePath: $ZfModulePath"
+}
+$splat.Add("Name", $ZfModulePath)
+# Ensure only 1 version of the module is loaded
+Get-Module ZeroFailed | Remove-Module
+Import-Module @splat
+$ver = "{0} {1}" -f (Get-Module ZeroFailed).Version, (Get-Module ZeroFailed).PrivateData.PsData.PreRelease
+Write-Host "Using ZeroFailed module version: $ver"
+#endregion
+
+$PSModuleAutoloadingPreference = 'none'
+
+# Load the build configuration
+. $here/.zf/config.ps1
